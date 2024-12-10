c41d2dd771db9b4b1e17f1f491055af988b41d39
Aakash <legendarypowers07@gmail.com>
2024-12-10 21:28:27 +0530

    Add files via upload

---
+++ Landing Page/script.js
@@ -0,0 +1,11 @@
+document.querySelector('.signin').addEventListener('click', () => {
+    alert('Sign-in functionality coming soon!');
+});
+
+document.querySelector('.try-now').addEventListener('click', () => {
+    alert('Try Now button clicked!');
+});
+
+document.querySelector('.contact-us').addEventListener('click', () => {
+    alert('Know Us button clicked!');
+});
